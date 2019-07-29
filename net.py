e06371083a502e53de9dd80c2052b6d15e9e0457
Jakub Korthals <korthalsjakub@gmail.com>
2019-07-29 15:45:50 +0200

    Bug fix

--- net.py
+++ net.py
@@ -13,27 +13,25 @@ class Net:
         for i in range(1,self.L):
             self.W.append(np.random.randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1]))
             self.b.append(np.zeros((layers[i], 1)))
+            self.A_saved.append(-1)
         self.W[-1] = self.W[-1] * 0.5   # different activation function in last layer
 
     def print_me(self):
-        print(self.W[-1])
-        print(type(self.W))
+        print(len(self.b[-1]))
 
     @staticmethod
     def activation(Z, type):
         if type == 'relu':
-            if Z > 0:
-                return Z
-            else:
-                return 0
+            return np.maximum(0,Z)
         elif type == 'sigmoid':
             return 1/(1+np.exp(-Z))
         elif type == 'softmax':
             temp = np.exp(Z)
             return temp/np.sum(temp)
 
-    def train(self):
-        #forward_prop(X,Y)
+
+    def train(self, X, Y):
+        self.forward_prop(X)
         #cost_function()
         #backward_prop()
         #parameters_update()
@@ -42,4 +40,7 @@ class Net:
 if __name__ == '__main__':
     #np.random.seed(1)
     test1 = Net((5,4,1))
+    X = np.array([0.05, 11, 100, 2, 0.8], ndmin=2)
+    X = X.T
+    #test1.forward_prop(X)
     test1.print_me()
